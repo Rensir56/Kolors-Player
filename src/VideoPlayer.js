12c77a4f0932ab749366966816781e9f918542f7
Rensir56 <3041294829@qq.com>
2024-09-10 12:54:41 +0800

    code base reset

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -6,7 +6,7 @@ const VideoPlayer = () => {
   return (
     <div className="video-container">
       <video controls loop muted>
-        <source src="/video.mp4" type="video/mp4" />
+        <source src="/video1.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
